410d73b72c949df49ec68804e681063d089d4769
Keren Cohen <keren5005005@gmail.com>
2026-05-05 12:30:27 +0300

    updating the protofolio website

--- src/components/ParticleBackground.js
+++ src/components/ParticleBackground.js
@@ -10,8 +10,8 @@ const ParticleBackground = () => {
 
   const iconsArray = Object.keys(techIconMap).map((tech) => ({
     src: techIconMap[tech],
-    height: 20,
-    width: 20,
+    height: 28,
+    width: 28,
   }));
 
   return (
@@ -21,42 +21,26 @@ const ParticleBackground = () => {
       options={{
         fullScreen: { enable: false },
         particles: {
-          number: {
-            value: 50, // Adjust the number of particles
-          },
+          number: { value: 28 },
           shape: {
             type: 'image',
             image: iconsArray,
           },
           opacity: {
-            value: 0.7,
+            value: 0.18,
+            random: { enable: true, minimumValue: 0.08 },
           },
           size: {
-            value: 20,
-            random: true,
+            value: 22,
+            random: { enable: true, minimumValue: 14 },
           },
           move: {
             enable: true,
-            speed: 1.5,
-            outMode: 'bounce',
-          },
-          // Configure particle movement so they avoid the central area
-          move: {
-            enable: true,
-            speed: 1.5,
+            speed: 0.8,
             direction: 'none',
             outMode: 'bounce',
             random: true,
             straight: false,
-            // Ensure they only move at the edges
-            bounce: false,
-            avoid: {
-              area: {
-                x: 0.5,  // Avoid the central horizontal area (50%)
-                y: 0.5,  // Avoid the central vertical area (50%)
-                radius: 200, // Increase or decrease this to make the safe zone bigger or smaller
-              },
-            },
           },
         },
         detectRetina: true,
--- src/components/TechIcons.js
+++ src/components/TechIcons.js
@@ -1,37 +1,32 @@
 import React from 'react';
 
 const techIconMap = {
-  "C#": "https://simpleicons.org/icons/csharp.svg",
-  "TypeScript": "https://simpleicons.org/icons/typescript.svg",
-  "JavaScript": "https://simpleicons.org/icons/javascript.svg",
-  "C++": "https://simpleicons.org/icons/cplusplus.svg",
-  "C": "https://simpleicons.org/icons/c.svg",
-  "Python": "https://simpleicons.org/icons/python.svg",
-  "Go": "https://simpleicons.org/icons/go.svg",
-  "SQL": "https://simpleicons.org/icons/postgresql.svg", 
-  "Docker": "https://simpleicons.org/icons/docker.svg",
-  "React": "https://simpleicons.org/icons/react.svg",
-  "Node.js": "https://simpleicons.org/icons/nodedotjs.svg",
-  "GIT": "https://simpleicons.org/icons/git.svg",
-  "Linux": "https://simpleicons.org/icons/linux.svg",
-  "CSS": "https://simpleicons.org/icons/css3.svg",
-  "HTML": "https://simpleicons.org/icons/html5.svg",
-  "REST APIs": "https://simpleicons.org/icons/api.svg",
-  "Unity": "https://simpleicons.org/icons/unity.svg",
-  "Prometheus": "https://simpleicons.org/icons/prometheus.svg",
-  "Wireshark": "https://simpleicons.org/icons/wireshark.svg",
+  "TypeScript":  "https://cdn.simpleicons.org/typescript/94a3b8",
+  "JavaScript":  "https://cdn.simpleicons.org/javascript/94a3b8",
+  "Python":      "https://cdn.simpleicons.org/python/94a3b8",
+  "Go":          "https://cdn.simpleicons.org/go/94a3b8",
+  "C#":          "https://cdn.simpleicons.org/csharp/94a3b8",
+  "C++":         "https://cdn.simpleicons.org/cplusplus/94a3b8",
+  "Docker":      "https://cdn.simpleicons.org/docker/94a3b8",
+  "React":       "https://cdn.simpleicons.org/react/94a3b8",
+  "Node.js":     "https://cdn.simpleicons.org/nodedotjs/94a3b8",
+  "PostgreSQL":  "https://cdn.simpleicons.org/postgresql/94a3b8",
+  "MongoDB":     "https://cdn.simpleicons.org/mongodb/94a3b8",
+  "Redis":       "https://cdn.simpleicons.org/redis/94a3b8",
+  "Linux":       "https://cdn.simpleicons.org/linux/94a3b8",
+  "Git":         "https://cdn.simpleicons.org/git/94a3b8",
+  "Kafka":       "https://cdn.simpleicons.org/apachekafka/94a3b8",
+  "Prometheus":  "https://cdn.simpleicons.org/prometheus/94a3b8",
+  "Kubernetes":  "https://cdn.simpleicons.org/kubernetes/94a3b8",
+  "GitHub":      "https://cdn.simpleicons.org/github/94a3b8",
 };
 
 const TechIcons = () => (
   <div className="flex flex-wrap justify-center">
     {Object.keys(techIconMap).map((tech) => (
       <div key={tech} className="m-4 text-center">
-        <img
-          src={techIconMap[tech]}
-          alt={`${tech} Logo`}
-          className="w-12 h-12"
-        />
-        <p className="mt-2">{tech}</p>
+        <img src={techIconMap[tech]} alt={`${tech} Logo`} className="w-12 h-12" />
+        <p className="mt-2 text-sm text-zinc-400">{tech}</p>
       </div>
     ))}
   </div>
